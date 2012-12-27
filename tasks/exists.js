7ba17a40660fcc823b16df82c63fa3f18538dede
Jared Stehler <jared.stehler@cengage.com>
2012-12-27 02:43:26 -0500

    fixed issue, cleanup

--- tasks/exists.js
+++ tasks/exists.js
@@ -1,20 +1,19 @@
 module.exports = function(grunt) {
-  
-  // Nodejs libs.
-  var fs = require('fs');
-  var path = require('path');
-  // In Nodejs 0.8.0, existsSync moved from path -> fs.
-  var existsSync = fs.existsSync || path.existsSync;
 
-  
-  grunt.registerTask('exists', 'File Existence', function() {
-    var files = grunt.config('exists.files'); 
-  
-    grunt.utils._.each(this.data, function(file){
-      if(!existsSync(file)){
-        grunt.fatal("Required file [" + file + "] doesn't exist.");
-	  }
-	});
-  });
+    // Nodejs libs.
+    var fs = require('fs');
+    var path = require('path');
+    // In Nodejs 0.8.0, existsSync moved from path -> fs.
+    var existsSync = fs.existsSync || path.existsSync;
+
+    grunt.registerMultiTask('exists', 'File Existence', function() {
+        grunt.utils._.each(this.data, function(file) {
+            if (!existsSync(file)) {
+                grunt.fatal("Required file [" + file + "] doesn't exist.");
+            } else {
+                grunt.log.ok("Verified that required file [" + file + "] exists.");
+            }
+        });
+    });
 };
 
